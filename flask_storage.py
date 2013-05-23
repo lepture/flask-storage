83b4f201d89ad7afb203371d2b6116fe32d03ae3
Hsiaoming Yang <me@lepture.com>
2013-05-23 09:58:58 +0800

    sketch up storage

--- flask_storage.py
+++ flask_storage.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 
+import os
+from urlparse import urljoin
+from werkzeug import secure_filename, FileStorage
 
 TEXT = ('txt')
 
@@ -28,21 +31,84 @@ EXECUTABLES = ('so', 'ext', 'dll')
 
 
 class BaseStorage(object):
-    def __init__(self, name='base', extensions=None, config=None):
+    def __init__(self, name='base', extensions=IMAGES, config=None):
         self.name = name
         self.config = config
-        if extensions is None:
-            # default is images
-            self.extensions = IMAGES
-        else:
-            self.extensions = extensions
+        self.extensions = extensions
 
     def url(self, filename):
         """This function gets the URL a filename."""
         raise NotImplementedError
 
     def extension_allowed(self, extname):
+        if not self.extensions:
+            return True
         return extname in self.extensions
 
-    def save(self, storage, destination):
+    def check(self, storage):
+        """
+        Check if the storage can be saved.
+
+        :param storage: The storage to be saved.
+
+        This function should be called everytime when you want to
+        save a storage::
+
+            class DemoStorage(BaseStorage):
+                def save(self, storage, filename):
+                    # check storage before saving it
+                    self.check(storage)
+        """
+        if not isinstance(storage, FileStorage):
+            raise TypeError('storage must be a werkzeug.FileStorage')
+
+        _, extname = os.path.splitext(secure_filename(storage.filename))
+        ext = extname.lower()[1:]
+        if not self.extension_allowed(ext):
+            raise UploadNotAllowed()
+
+    def save(self, storage, filename):
         raise NotImplementedError
+
+
+class LocalStorage(BaseStorage):
+    @property
+    def root(self):
+        return self.config.get('STORAGE_LOCAL_ROOT')
+
+    def url(self, filename):
+        urlbase = self.config.get('STORAGE_LOCAL_URL')
+        return urljoin(urlbase, filename)
+
+    def save(self, storage, filename):
+        self.check(storage)
+
+
+class S3Storage(BaseStorage):
+    def request(self):
+        pass
+
+    def url(self, filename):
+        urlbase = self.config.get('STORAGE_S3_URL')
+        return urljoin(urlbase, filename)
+
+    def save(self, storage, filename):
+        self.check(storage)
+        pass
+
+
+class UpyunStorage(BaseStorage):
+    def request(self):
+        pass
+
+    def url(self, filename):
+        urlbase = self.config.get('STORAGE_UPYUN_URL')
+        return urljoin(urlbase, filename)
+
+    def save(self, storage, filename):
+        self.check(storage)
+        pass
+
+
+class UploadNotAllowed(Exception):
+    """This exception is raised if the upload was not allowed."""
